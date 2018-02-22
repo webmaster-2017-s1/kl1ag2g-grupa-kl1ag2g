055054e77794fc8dc1d18497ddc5c52b85e7c72b
pwojton <piotr.wojton.01@gmail.com>
2018-02-22 15:55:23 +0100

    dodanie mechanizmu pojawiania się jedzenia

--- js/app_p5.js
+++ js/app_p5.js
@@ -2,16 +2,30 @@
 var s;
 var scl = 20;
 
+var jedzenie;
 
 function setup() {
   createCanvas(600, 600);
   s = new Snake();
+  frameRate(10);                             //spowalnianie wężą
+  jedzenie = createVector(random(width), random(height));
 }
+
+function pickLocation() {
+  var cols = floor(width/scl);
+  var rows = floor(height/scl);
+  jedzenie = createVector(floor(random(cols)), floor(random(rows)));
+  food.mult(scl);
+}
+
 
 function draw() {
   background(255, 204, 0);
   s.update();
   s.show();
+
+  fill(255, 0, 100);
+  rect(jedzenie.x, jedzenie.y, scl, scl);  //pojawianie się jedzenia
 }
 
 function keyPressed() {                       //regowanie na wciśnięty kalwisz
