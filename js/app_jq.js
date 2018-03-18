86baa88bfd6415016bcf2d6249e90cd63fca4795
zbrsnd <zbrsnd@gmail.com>
2018-03-18 23:20:34 +0100

    dodanie nowego logo i kontynuacja pracy nad wersją JQ

--- js/app_jq.js
+++ js/app_jq.js
@@ -1,44 +1,72 @@
 // kod projektu [wpisz nazwę]
 $(document).ready(function(){
-//dostosowanie canvasu
+	
+  var field = $("#field");
+	var ctx = canvas.getContext("2d");
+	var w = $("#canvas").width();
+	var h = $("#canvas").height();
+  
+  
+	//Lets save the cell width in a variable for easy control
+	var cw = 10;
+	var d;
+	var food;
+	var score;
+	
+	//tworzenie snake'a
+	var snake_array; //tablica tworząca snake'a
+	
+	function init()
+	{
+		d = "right"; //domyślny kierunek
+		create_snake();
+		create_food(); 
+		
+		
+		//ruszanie snake'a za pomocą timera
+		if(typeof game_loop != "undefined") clearInterval(game_loop);
+		game_loop = setInterval(paint, 60);
+	}
+	init();
+	
+	function create_snake()
+	{
+		var length = 5; //dlugosc snake'a
+		snake_array = []; //pusta poczatkowa tablica
+		for(var i = length-1; i>=0; i--)
+		{
+			//tworzenie snake'a w lewym gornym rogu
+			snake_array.push({x: i, y:0});
+		}
+	}
+	
+    //stworzenie jedzenia
+	function create_food()
+	{
+		food = {
+			x: Math.round(Math.random()*(w-cw)/cw), 
+			y: Math.round(Math.random()*(h-cw)/cw), 
+		};
+		
+	}
+	
+	//rysowanie snake'a
+	function paint()
+	{
+		//rysowanie tla w kazdej klatce
+		//rysowanie canvasu
+		ctx.fillStyle = "rgb(255, 204, 0)";
+		ctx.fillRect(0, 0, w, h);
+		ctx.strokeStyle = "black";
+		ctx.strokeRect(0, 0, w, h);
+		
+		
+		var nx = snake_array[0].x;
+		var ny = snake_array[0].y;
+		//Pozycja "głowy"
 
-var canvas = $("#canvas")[0];
-var ctx = canvas.getContext("2d");
-var w = $("#canvas").width();
-var h = $("#canvas").height();
-
-//rysowanie canvasu
-
-ctx.fillStyle = "rgb(255, 204, 0)";
-ctx.fillRect(0,0,w,h);
-ctx.strokeStyle = "black";
-ctx.strokeRect(0,0,w,h);
-
-//tworzenie snake'a
-
-var snake_array; //tablica komórek potrzebnych, by zbudować snake'a
-
-function create_snake() {
-    var length = 5; //dlugosc snake'a
-    snake_array = []; //pusta, poczatkowa tablica
-
-    for(var i = length-1; i>=0; i--) {
-        snake_array.push({x: i, y:0}); //stworzenie snake'a na poczatkowej pozycji w lewym gornym rogu
-    }
-}
-
-//rysowanie snake'a
-function rysuj(){
-
-    for(var i = 0; i < snake_array.length; i++){
-        var c = snake_array[i];
-        ctx.fillStyle = "blue"; //komorki, z ktorych zbudowany jest snake
-        ctx.fillRect(c.x*10, c.y*10, 10, 10);
-        ctx.strokeStyle = "black";
-        ctx.strpleRect(c.x*10, c.y*10, 10, 10);
-
-    }
-}
-
-    //;
-});
+        
+		if(d == "right") nx++;
+		else if(d == "left") nx--;
+		else if(d == "up") ny--;
+		else if(d == "down") ny++;
